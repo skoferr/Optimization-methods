a61feec9bad633836ff0332387e925ce30a92ce2
skoferr <160164549+skoferr@users.noreply.github.com>
2025-03-22 15:20:58 +0300

    Update chord.cpp

--- chord.cpp
+++ chord.cpp
@@ -61,7 +61,7 @@ double chord_method(double a, double b, double e) {
 
 
 int main() {
-    setlocale(LC_ALL, "RU"); // Устанавливаем русскую локализацию
+    setlocale(LC_ALL, "RU");
     double a, b; // Интервал поиска минимума (в случае нашей задачи: [-3; 0.5])
     double e; // Точность (в нашей задаче: 0.06)
     // Ввод границ и точности с клавиатуры
