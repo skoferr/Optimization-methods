c54ab647022dfec52dcc470966dbe02f699397d7
skoferr <160164549+skoferr@users.noreply.github.com>
2025-03-22 05:23:20 +0300

    Add files via upload

---
+++ bitwise search.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int max_iter = 150;
+
+// Исходная функция
+double function(double x) {
+    return x * exp(-(x * x) / 2);
+}
+
+// Метод поразрядного поиска минимума
+double bitwiseSearch(double a, double b, double e) {
+    double step = (b - a) / 4; //Дельта
+    double x0 = a;//Начальная / левая граница
+    double x_new, f0, f_new;
+    int iter = 0;
+    f0 = function(x0);
+    while (step > e) {
+
+        cout << endl << "Вправо" << endl;
+
+        while (true) { //Цикл двигается вправо
+            x_new = x0 + step;
+
+            f_new = function(x_new);
+
+            cout << "Итерация |" << iter << "| " << "| step " << step << " | x = " << x_new << " | f(x) " << f_new << endl;
+            iter++;
+            if (iter == max_iter) return x0;
+            if (f0 > f_new) {
+                x0 = x_new;
+                f0 = f_new;
+
+                if (x0 < a)
+                    x0 = a;
+                else if (x0 >= b)
+                    x0 = b;
+
+            }
+            else {
+                break;
+            }
+        }
+        step /= 4; //Уменьшение шага
+
+        x0 = x_new;
+        f0 = function(x0);
+
+        cout << endl << "Влево" << endl;
+
+        while (true) //Цикл движется влево
+        {
+            x_new = x0 - step;
+            f_new = function(x_new);
+
+            cout << "Итерация | " << iter << " | " << " | step " << step << " | x = " << x_new << " | f(x) " << f_new << endl;
+            iter++;
+            if (iter == max_iter) return x0;
+
+            if (f0 > f_new) {
+                b = x0;
+                x0 = x_new;
+                f0 = f_new;
+
+                if (x0 < a)
+                    x0 = a;
+                else if (x0 > b)
+                    x0 = b;
+            }
+            else {
+                break;
+            }
+
+
+        }
+    }
+    return x0;
+    cout << "\n\nИтераций: " << iter << endl;
+}
+int main() {
+    setlocale(LC_ALL, "RU"); // Устанавливаем русскую локализацию
+    double a, b; // Интервал поиска минимума (в случае нашей задачи: [-8; 0.5])
+    double e; // Точность (в нашей задаче: 0.06)
+    // Ввод границ и точности с клавиатуры
+    cout << "Введите левую границу: ";
+    cin >> a;
+    cout << "Введите правую границу: ";
+    cin >> b;
+    cout << "Введите точность: ";
+    cin >> e;
+    cout << endl;
+    double min_x;
+    min_x = bitwiseSearch(a, b, e);
+    cout << "x = " << min_x << " f(x) = " << function(min_x) << endl;
+    system("pause");
+    system("cls");
+    main();
+}
---
+++ dihotomy.cpp
@@ -0,0 +1,64 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int max_iter = 150;
+
+// Функция для вычисления значения целевой функции
+double function(double x) {
+    return x * exp(-(x * x) / 2);
+}
+
+
+
+// Метод дихотомии для поиска минимума функции
+void dichotomy(float a, float b, float e) {
+    int iter = 0; // Счетчик числа итераций
+
+    while (true) {
+        iter++;
+        if (iter == max_iter) {
+            cout << "\n\nИтераций: " << iter << "\nx = " << (a + b) / 2 << " f(x) = " << function((a + b) / 2) << endl;
+            break;
+        }
+        float en = (b - a) / 2; // Половина текущего отрезка
+        if (en <= e) { // Критерий завершения алгоритма
+            cout << "\n\nИтераций: " << iter << "\nx = " << (a + b) / 2 << " f(x) = " << function((a + b) / 2) << endl;
+            break;
+        }
+
+        float x1 = (a + b - e) / 2;
+        float x2 = (a + b + e) / 2;
+
+        // Вывод текущего состояния интервала и вычисленных точек
+        cout << "Итерация |" << iter << "| a = " << a << " | b = " << b << " | x1 = " << x1
+            << " | x2 = " << x2 << " | f(x1) = " << function(x1) << " | f(x2) = " << function(x2) << endl;
+
+        // Уточнение границ интервала
+        if (function(x1) <= function(x2)) {
+            b = x2; // Сужаем интервал к [a, x2]
+        }
+        else {
+            a = x1; // Сужаем интервал к [x1, b]
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "RU"); // Устанавливаем русскую локализацию
+    double a, b; // Интервал поиска минимума (в случае нашей задачи: [-8; 0.5])
+    double e; // Точность (в нашей задаче: 0.06)
+    // Ввод границ и точности с клавиатуры
+    cout << "Введите левую границу: ";
+    cin >> a;
+    cout << "Введите правую границу: ";
+    cin >> b;
+    cout << "Введите точность: ";
+    cin >> e;
+    cout << endl;
+    dichotomy(a, b, e);
+    system("pause");
+    system("cls");
+    main();
+}
